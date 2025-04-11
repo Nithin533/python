45017f6e949a335acf3b492ef4c8fc6cdb2c54fa
Nithin533 <nithinravibca24@rvu.edu.in>
2025-04-11 13:00:04 +0530

    Create a1.py

---
+++ Activity/a1.py
@@ -0,0 +1,14 @@
+def calculator(num1,num2):
+	num1=int(input("enter the number"))
+	num2=int(input("enter the number"))
+	choice=input("enter the choice")
+	if (choice == "%"):
+		print(num1%num2)
+	elif (choice == "/"):
+		print(num1/num2)
+	elif (choice == "**"):
+		print(num1**num2)
+	else:
+		return num1+num2
+	
+calculator()
